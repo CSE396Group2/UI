371aa3e80ead8030e91be42ff820b22447126b4b
emrullahgencoglu <emrullahgencoglu@gtu.edu.tr>
2017-05-03 17:50:28 +0300

    timer added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+/*
+#include <QCamera>
+#include <QCameraViewfinder>
+#include <QCameraImageCapture>
+#include <QVBoxLayout>
+#include <QMenu>
+#include <QAction>
+#include <QFileDialog>*/
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+  //  cameraView();
+
 }
 
 MainWindow::~MainWindow()
@@ -11,6 +22,70 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::MyTimer()
+{
+
+    // create a timer
+    timer = new QTimer(this);
+
+    // setup signal and slot
+    connect(timer, SIGNAL(timeout()),
+          this, SLOT(MyTimerSlot()));
+    // msec
+    timer->start(1);
+}
+void MainWindow::MyTimerSlot()
+{
+
+    count++;
+    ui->Timer->setText(QString::number((count/100)/60)+":"+QString::number((count/100)%60)+":"+QString::number(count%100));
+
+}
+/*void MainWindow::cameraView(){
+
+    mCamera = new QCamera(this);
+    mCameraImageCapture = new QCameraImageCapture(mCamera,this);
+    mCameraViewfinder = new QCameraViewfinder(this);
+    mLayout = new QVBoxLayout;
+    mOptionsMenu = new QMenu("Options",this);
+    mTurnAction = new QAction("TurnOn",this);
+    mDeleteAction = new QAction("TurnOff",this);
+    mCaptureAction = new QAction("Capture",this);
+
+    mOptionsMenu->addActions({mTurnAction,mDeleteAction,mCaptureAction});
+    ui->optionsPushButton->setMenu(mOptionsMenu);
+    mCamera->setViewfinder(mCameraViewfinder);
+    mLayout->addWidget(mCameraViewfinder);
+    mLayout->setMargin(0);
+    //ui->scrollArea->setLayout(mLayout);
+    ui->openGLWidget->setLayout(mLayout);
+    connect(mTurnAction,&QAction::triggered,[&](){
+        mCamera->start();
+    });
+
+    connect(mDeleteAction,&QAction::triggered,[&](){
+         mCamera->stop();
+    });
+    connect(mCaptureAction,&QAction::triggered,[&](){
+        auto filename = QFileDialog::getSaveFileName(this,"Capture","/","Imagen(*.jpg;jpeg)");
+
+        if(filename.isEmpty()){
+            return;
+        }
+
+        mCameraImageCapture->setCaptureDestination(
+                    QCameraImageCapture::CaptureToFile);
+        QImageEncoderSettings imageEncoderSettings;
+        imageEncoderSettings.setCodec("image/jpeg");
+        imageEncoderSettings.setResolution(1600,1200);
+        mCameraImageCapture->setEncodingSettings(imageEncoderSettings);
+        mCamera->setCaptureMode(QCamera::CaptureStillImage);
+        mCamera->start();
+        mCamera->searchAndLock();
+        mCameraImageCapture->capture(filename);
+        mCamera->unlock();
+    });
+}*/
 void MainWindow::startServer(){
     qDebug() << "Server started" ;
 }
@@ -23,6 +98,7 @@ void MainWindow::on_startButton_clicked()
         strcpy(onClickedMessage,"message");
         //MyTimer myTimer();
         qDebug() << "startButton was clicked";
+        MyTimer();
         ui->coordinatBrowser->append("gurol");
 
     }
@@ -32,6 +108,7 @@ void MainWindow::on_stopButton_clicked()
 {
     if(isStopButtonClicked == false){
         isStartButtonClicked = false;
+         ui->Timer->setText("Stoped");
         isStopButtonClicked = true;
         qDebug() << "stopButton was clicked";
     }
--- mainwindow.h
+++ mainwindow.h
@@ -3,7 +3,15 @@
 
 #include <QMainWindow>
 #include <QDebug>
-
+#include <QTimer>
+/*
+class QCamera;
+class QCameraViewfinder;
+class QCameraImageCapture;
+class QVBoxLayout;
+class QMenu;
+class QAction;
+*/
 namespace Ui {
 class MainWindow;
 }
@@ -13,20 +21,37 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
+  /*  QCamera *mCamera;
+    QCameraViewfinder *mCameraViewfinder;
+    QCameraImageCapture *mCameraImageCapture;
+    QVBoxLayout *mLayout;
+    QMenu *mOptionsMenu;
+    QAction *mTurnAction;
+    QAction *mDeleteAction;
+    QAction *mCaptureAction;
+*/
+    QTimer *timer;
+    int count=0;
     char onClickedMessage[64];
     bool isStartButtonClicked = false;
     bool isStopButtonClicked = false;
+
 private slots:
+
+    void MyTimer();
+    void MyTimerSlot();
+    //void cameraView();
     void on_startButton_clicked();
     void startServer();
     void on_stopButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+
 };
 
 #endif // MAINWINDOW_H
